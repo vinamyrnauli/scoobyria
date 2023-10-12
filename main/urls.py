3eac3fe99fe1f2b379544cb1b9b2dbd3e05e6db5
vinamyrnauli <vinasllgn@gmail.com>
2023-10-12 20:12:06 +0700

    NOT FINISHED (TUGAS 6)

--- main/urls.py
+++ main/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from main.views import show_main, create_product, add_product, decrement_product, remove_product, show_xml, show_json, show_xml_by_id, show_json_by_id
+from main.views import show_main, create_product, add_product, decrement_product, remove_product, show_xml, show_json, show_xml_by_id, show_json_by_id, get_product_json, add_product_ajax
 from main.views import register
 from main.views import login_user
 from main.views import logout_user
@@ -10,15 +10,17 @@ app_name = 'main'
 urlpatterns = [
     path('', show_main, name='show_main'),
     path('create-product', create_product, name='create_product'),
-    path('add_product/<int:product_id>/', add_product, name='add_product'),
-    path('decrement_product/<int:product_id>/', decrement_product, name='decrement_product'),
-    path('remove_product/<int:product_id>/',
+    path('add-product/<int:product_id>/', add_product, name='add_product'),
+    path('decrement-product/<int:product_id>/', decrement_product, name='decrement_product'),
+    path('remove-product/<int:product_id>/',
     remove_product, name='remove_product'),
     path('edit-product/<int:id>', edit_product, name='edit_product'),
     path('xml/', show_xml, name='show_xml'), 
     path('json/', show_json, name='show_json'), 
     path('xml/<int:id>/', show_xml_by_id, name='show_xml_by_id'),
     path('json/<int:id>/', show_json_by_id, name='show_json_by_id'), 
+    path('get-product/', get_product_json, name='get_product_json'),
+    path('create-product-ajax/', add_product_ajax, name='add_product_ajax'),
     path('register/', register, name='register'),
     path('login/', login_user, name='login'),
     path('logout/', logout_user, name='logout'),
--- main/views.py
+++ main/views.py
@@ -1,5 +1,5 @@
 import datetime
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, HttpResponseNotFound
 from django.urls import reverse
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import logout
@@ -58,24 +58,21 @@ def edit_product(request, id):
 
 # BONUS
 def add_product(request, product_id):
-    if request.method == 'POST' and 'Tambah' in request.POST:
-        product = Product.objects.get(id = product_id)
-        product.amount += 1
-        product.save()
+    product = Product.objects.get(id = product_id)
+    product.amount += 1
+    product.save()
     return HttpResponseRedirect(reverse('main:show_main'))
 
 def decrement_product(request, product_id):
-    if request.method == 'POST' and 'Kurang' in request.POST:
-        product = Product.objects.get(id = product_id)
-        if product.amount > 0 :
-            product.amount -= 1
-        product.save()
+    product = Product.objects.get(id = product_id)
+    if product.amount > 1 :
+        product.amount -= 1
+    product.save()
     return HttpResponseRedirect(reverse('main:show_main'))
 
 def remove_product(request, product_id):
-    if request.method == 'POST' and 'Hapus' in request.POST:
-        product = Product.objects.get(id = product_id)
-        product.delete()
+    product = Product.objects.get(id = product_id)
+    product.delete()
     return HttpResponseRedirect(reverse('main:show_main'))
 
 def show_xml(request):
@@ -105,6 +102,26 @@ def register(request):
             return redirect('main:login')
     context = {'form':form}
     return render(request, 'register.html', context)
+
+def get_product_json(request):
+    product_item = Product.objects.filter(user=request.user)
+    return HttpResponse(serializers.serialize('json', product_item))
+
+#@csrf_exempt
+def add_product_ajax(request):
+    if request.method == 'POST':
+        name = request.POST.get("name")
+        price = request.POST.get("price")
+        amount = request.POST.get("amount")
+        description = request.POST.get("description")
+        user = request.user
+
+        new_product = Product(name=name, price=price, amount=amount, description=description, user=user)
+        new_product.save()
+
+        return HttpResponse(b"CREATED", status=201)
+
+    return HttpResponseNotFound()
 
 def login_user(request):
     if request.method == 'POST':
